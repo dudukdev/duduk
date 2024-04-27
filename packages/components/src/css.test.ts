c82bffb71d3c63597246b91533c4cbf3c65a7eea
Veit Kunz <git@veit-kunz.de>
2024-04-27 22:03:27 +0200

    Add Vitest and first unit test

---
+++ packages/components/src/css.test.ts
@@ -0,0 +1,53 @@
+import {css} from "./css";
+import {expect, test} from "vitest";
+
+test("return correct styles", async () => {
+  const styles = css`
+    .something {
+      color: red;
+    }
+  `;
+
+  expect(styles instanceof HTMLStyleElement).toBeTruthy();
+  expect(styles.textContent).toEqual(`
+    .something {
+      color: red;
+    }
+  `);
+});
+
+test("return correct styles if it contains variables", async () => {
+  const color = 'red';
+  const styles = css`
+    .something {
+      color: ${color};
+    }
+  `;
+
+  expect(styles instanceof HTMLStyleElement).toBeTruthy();
+  expect(styles.textContent).toEqual(`
+    .something {
+      color: red;
+    }
+  `);
+});
+
+test("return correct styles if 'prependStyles' is set", async () => {
+  window.__app = {
+    prependStyles: '.prependStyles {font-size: 1em}'
+  }
+
+  const styles = css`
+    .something {
+      color: red;
+    }
+  `;
+
+  expect(styles instanceof HTMLStyleElement).toBeTruthy();
+  expect(styles.textContent).toEqual(`.prependStyles {font-size: 1em}
+
+    .something {
+      color: red;
+    }
+  `);
+});
--- packages/components/src/html.ts
+++ packages/components/src/html.ts
@@ -1,9 +1,9 @@
 export function html(strings: TemplateStringsArray, ...values: any[]): DocumentFragment {
-    let result = strings[0];
-    for (let i = 1; i < strings.length; i++) {
-        result += values[i - 1] + strings[i];
-    }
-    const template = document.createElement('template');
-    template.innerHTML = result;
-    return template.content;
+  let result = strings[0];
+  for (let i = 1; i < strings.length; i++) {
+    result += values[i - 1] + strings[i];
+  }
+  const template = document.createElement('template');
+  template.innerHTML = result;
+  return template.content;
 }
