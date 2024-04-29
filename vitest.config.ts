40add7782885b6180cbd2f2c26f04cd3e8d7765b
Veit Kunz <git@veit-kunz.de>
2024-04-30 01:20:13 +0200

    Add unit tests to pipeline

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,17 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    reporters: [
+      'default',
+      ['junit', {outputFile: 'reports/junit.xml'}]
+    ],
+    coverage: {
+      provider: 'v8',
+      reporter: [
+        'text',
+        'text-summary',
+        'cobertura'
+      ]
+    }
   },
 })
